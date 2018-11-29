fc3875bb609ba0772316727f11bcb2d4914f9037
minhvuong <minhvuong21101996@gmail.com>
2018-11-29 23:57:18 +0700

    29/11

---
+++ binhchanh/application/views/header_parts/header.php
@@ -0,0 +1,154 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <title>Faith - Church Template | Home</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="http://localhost/mycode/binhchanh/public/img/core-img/favicon.ico">
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="http://localhost/mycode/binhchanh/public/style.css">
+
+</head>
+
+<body>
+    <!-- ##### Search Wrapper Start ##### -->
+    <div class="search-wrapper d-flex align-items-center justify-content-center bg-img foo-bg-overlay" style="background-image: url(img/bg-img/bg-2.jpg);">
+       <div class="close--icon">
+           <i class="fa fa-times"></i>
+       </div>
+        <!-- Logo -->
+        <a href="index.html" class="search-logo"><img src="/public/img/core-img/logo2.png" alt=""></a>
+        <!-- Search Form -->
+        <div class="search-form">
+            <form action="#" method="get">
+                <input type="search" name="search" id="search" placeholder="Enter Your Keywords">
+                <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+            </form>
+        </div>
+        <!-- Copwrite Text -->
+        <div class="copywrite-text">
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+        </div>
+    </div>
+    <!-- ##### Search Wrapper End ##### -->
+
+    <!-- ##### Preloader ##### -->
+    <div id="preloader">
+        <div class="circle">
+            <img src="img/core-img/church.png" alt="">
+        </div>
+    </div>
+
+    <!-- ##### Header Area Start ##### -->
+    <header class="header-area">
+        <!-- Top Header Area -->
+        <div class="top-header">
+            <div class="container h-100">
+                <div class="row h-100">
+                    <div class="col-12 h-100">
+                        <div class="top-header-content h-100 d-flex align-items-center justify-content-end">
+                            <!-- Next Events Countdown -->
+                            <div class="next-events-countdown d-flex align-items-center">
+                                <p>Next Big Event:</p>
+                                <div class="events-cd" data-countdown="2022/01/01"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Navbar Area -->
+        <div class="faith-main-menu">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="faithNav">
+
+                        <!-- Logo -->
+                        <a class="nav-brand" href="index.html"><img src="img/core-img/logo.png" alt=""></a>
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu">
+
+                            <!-- Close Button -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+
+                            <!-- Nav Start -->
+                            <div class="classynav">
+                                <ul>
+                                    <li><a href="index.html">Home</a></li>
+                                    <li><a href="#">Pages</a>
+                                        <ul class="dropdown">
+                                            <li><a href="index.html">Home</a></li>
+                                            <li><a href="ministries.html">Ministries</a></li>
+                                            <li><a href="sermons.html">Sermons</a></li>
+                                            <li><a href="blog.html">Blog</a></li>
+                                            <li><a href="contact.html">Contact</a></li>
+                                            <li><a href="elements.html">Elements</a></li>
+                                            <li><a href="#">Dropdown</a>
+                                                <ul class="dropdown">
+                                                    <li><a href="index.html">Home</a></li>
+                                                    <li><a href="ministries.html">Ministries</a></li>
+                                                    <li><a href="sermons.html">Sermons</a></li>
+                                                    <li><a href="blog.html">Blog</a></li>
+                                                    <li><a href="contact.html">Contact</a></li>
+                                                    <li><a href="elements.html">Elements</a></li>
+                                                    <li><a href="#">Dropdown</a>
+                                                        <ul class="dropdown">
+                                                            <li><a href="index.html">Home</a></li>
+                                                            <li><a href="ministries.html">Ministries</a></li>
+                                                            <li><a href="sermons.html">Sermons</a></li>
+                                                            <li><a href="blog.html">Blog</a></li>
+                                                            <li><a href="contact.html">Contact</a></li>
+                                                            <li><a href="elements.html">Elements</a></li>
+                                                        </ul>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="ministries.html">Ministries</a></li>
+                                    <li><a href="sermons.html">Sermons</a></li>
+                                    <li><a href="contact.html">Contact</a></li>
+                                </ul>
+
+                                <!-- Search Button -->
+                                <div class="search-btn">
+                                    <i class="fa fa-search" aria-hidden="true"></i>
+                                </div>
+
+                                <!-- Donate Button -->
+                                <div class="donate-btn">
+                                    <a href="#">Send Donations</a>
+                                </div>
+
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ##### Header Area End ##### -->
---
+++ binhchanh/application/views/ministries.php
@@ -0,0 +1,319 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+$this->load->view('header');
+?>
+
+    <!-- ##### Breadcumb Area Start ##### -->
+    <div class="breadcumb-area bg-img" style="background-image: url(img/bg-img/bg-4.jpg);">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12 col-md-6">
+                    <div class="breadcumb-text">
+                        <h5>Join one today</h5>
+                        <h2>Ministries</h2>
+                        <p>Cras iaculis eleifend arcu, non cursus sem. Morbi viverra varius nisl, ac varius mauris interdum sit amet. Aenean ac fermentum neque.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ##### Breadcumb Area End ##### -->
+
+    <!-- ##### Ministries Area Start ##### -->
+    <section class="ministries-area section-padding-100-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-heading text-center mx-auto">
+                        <img src="img/core-img/cross.png" alt="">
+                        <h3>The Ministries</h3>
+                        <p>Donec quis metus ac arcu luctus accumsan. Nunc in justo tincidunt, sodales nunc id, finibus nibh.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+
+                <!-- Single Ministry Area -->
+                <div class="col-12 col-md-6">
+                    <div class="single-ministry mb-100">
+                        <img src="img/bg-img/m1.jpg" alt="">
+                        <div class="ministry-content">
+                            <h6>Woman’s Ministry</h6>
+                            <span>8:00 pm @ Main hall</span>
+                            <p>Fusce nec ante vitae lacus aliquet vulputate. Donec scelerisque accumsan molestie. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras sed accumsan neque.</p>
+                            <div class="ministry-btn">
+                                <a href="#" class="btn faith-btn">See More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Ministry Area -->
+                <div class="col-12 col-md-6">
+                    <div class="single-ministry mb-100">
+                        <img src="img/bg-img/m2.jpg" alt="">
+                        <div class="ministry-content">
+                            <h6>Children’s Ministry</h6>
+                            <span>8:00 pm @ Main hall</span>
+                            <p>Fusce nec ante vitae lacus aliquet vulputate. Donec scelerisque accumsan molestie. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras sed accumsan neque.</p>
+                            <div class="ministry-btn">
+                                <a href="#" class="btn faith-btn">See More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Ministry Area -->
+                <div class="col-12 col-sm-6 col-lg-4">
+                    <div class="single-ministry mb-100">
+                        <img src="img/bg-img/m3.jpg" alt="">
+                        <div class="ministry-content">
+                            <h6>Couples’s Ministry</h6>
+                            <span>8:00 pm @ Main hall</span>
+                            <p>Fusce nec ante vitae lacus aliquet vulputate. Donec scelerisque accumsan molestie. Vestibulum ante ipsum primis.</p>
+                            <div class="ministry-btn">
+                                <a href="#" class="btn faith-btn">See More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Ministry Area -->
+                <div class="col-12 col-sm-6 col-lg-4">
+                    <div class="single-ministry mb-100">
+                        <img src="img/bg-img/m4.jpg" alt="">
+                        <div class="ministry-content">
+                            <h6>Family’s Ministry</h6>
+                            <span>8:00 pm @ Main hall</span>
+                            <p>Donec scelerisque accumsan molestie. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices.</p>
+                            <div class="ministry-btn">
+                                <a href="#" class="btn faith-btn">See More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Ministry Area -->
+                <div class="col-12 col-sm-6 col-lg-4">
+                    <div class="single-ministry mb-100">
+                        <img src="img/bg-img/m5.jpg" alt="">
+                        <div class="ministry-content">
+                            <h6>Single’s Ministry</h6>
+                            <span>8:00 pm @ Main hall</span>
+                            <p>Scelerisque accumsan molestie. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices.</p>
+                            <div class="ministry-btn">
+                                <a href="#" class="btn faith-btn">See More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- ##### Ministries Area End ##### -->
+
+    <!-- ##### Pastors Area Start ##### -->
+    <section class="pastors-area section-padding-100-0 bg-img bg-overlay" style="background-image: url(img/bg-img/bg-5.jpg);">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-heading white text-center mx-auto">
+                        <img src="img/core-img/cross.png" alt="">
+                        <h3>The Pastors</h3>
+                        <p>Donec quis metus ac arcu luctus accumsan. Nunc in justo tincidunt, sodales nunc id, finibus nibh.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <!-- Single Pastors Area -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-pastor-area mb-100">
+                        <img src="img/bg-img/pastor1.jpg" alt="">
+                        <div class="pastor-content">
+                            <h5>Cesar Williams</h5>
+                            <h6>Priest</h6>
+                            <div class="pastor-meta d-flex align-items-center justify-content-between">
+                                <a href="#">Ministries</a>
+                                <a href="#">Sermons</a>
+                                <a href="#">Contact</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Pastors Area -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-pastor-area mb-100">
+                        <img src="img/bg-img/pastor2.jpg" alt="">
+                        <div class="pastor-content">
+                            <h5>James Doe</h5>
+                            <h6>Priest</h6>
+                            <div class="pastor-meta d-flex align-items-center justify-content-between">
+                                <a href="#">Ministries</a>
+                                <a href="#">Sermons</a>
+                                <a href="#">Contact</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Pastors Area -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-pastor-area mb-100">
+                        <img src="img/bg-img/pastor3.jpg" alt="">
+                        <div class="pastor-content">
+                            <h5>Michael Smith</h5>
+                            <h6>Priest</h6>
+                            <div class="pastor-meta d-flex align-items-center justify-content-between">
+                                <a href="#">Ministries</a>
+                                <a href="#">Sermons</a>
+                                <a href="#">Contact</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Pastors Area -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-pastor-area mb-100">
+                        <img src="img/bg-img/pastor4.jpg" alt="">
+                        <div class="pastor-content">
+                            <h5>Steve Parker</h5>
+                            <h6>Priest</h6>
+                            <div class="pastor-meta d-flex align-items-center justify-content-between">
+                                <a href="#">Ministries</a>
+                                <a href="#">Sermons</a>
+                                <a href="#">Contact</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ##### Pastors Area End ##### -->
+
+    <!-- ##### CTA Area Start ##### -->
+    <div class="faith-cta-area style-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="cta-text d-flex justify-content-between align-items-center">
+                        <h5>Duis elementum diam eget eros tincidunt, at venenatis nunc tincidunt</h5>
+                        <a href="#" class="btn faith-btn active">Visitors Info</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ##### CTA Area End ##### -->
+
+    <!-- ##### Footer Area Start ##### -->
+    <footer class="footer-area">
+        <!-- Main Footer Area -->
+        <div class="main-footer-area section-padding-100-0 bg-img foo-bg-overlay" style="background-image: url(img/bg-img/bg-1.jpg);">
+            <div class="container">
+                <div class="row">
+
+                    <!-- Footer Widget Area -->
+                    <div class="col-12 col-sm-6 col-xl-3">
+                        <div class="d-flex flex-wrap mb-100">
+                            <div class="footer-widget">
+                                <div class="widget-title">
+                                    <a href="#"><img src="img/core-img/logo2.png" alt=""></a>
+                                </div>
+                                <div class="footer-social-info">
+                                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                                    <a href="#"><i class="fa fa-pinterest"></i></a>
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                </div>
+                            </div>
+                            <div class="copywrite-text">
+                                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Footer Widget Area -->
+                    <div class="col-12 col-sm-6 col-xl-3">
+                        <div class="footer-widget mb-100">
+                            <div class="widget-title">
+                                <h6>Contact Us</h6>
+                            </div>
+
+                            <!-- Single Contact Area -->
+                            <div class="single-contact-area mb-30">
+                                <p>Address:</p>
+                                <span>1481 Creekside Lane Avila <br>Beach, CA 93424 </span>
+                            </div>
+
+                            <!-- Single Contact Area -->
+                            <div class="single-contact-area mb-30">
+                                <p>Phone:</p>
+                                <span>+53 345 7953 32453 <br>+53 345 7557 822112</span>
+                            </div>
+
+                            <!-- Single Contact Area -->
+                            <div class="single-contact-area mb-30">
+                                <p>Email:</p>
+                                <span>yourmail@gmail.com</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Footer Widget Area -->
+                    <div class="col-12 col-sm-6 col-xl-3">
+                        <div class="footer-widget mb-100">
+                            <div class="widget-title">
+                                <h6>Usefull Links</h6>
+                            </div>
+                            <nav>
+                                <ul class="useful-links">
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">About Us</a></li>
+                                    <li><a href="#">Sermons</a></li>
+                                    <li><a href="#">Ministries</a></li>
+                                    <li><a href="#">Causes</a></li>
+                                    <li><a href="#">Testimonials</a></li>
+                                    <li><a href="#">Quotes</a></li>
+                                    <li><a href="#">News</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+
+                    <!-- Footer Widget Area -->
+                    <div class="col-12 col-sm-6 col-xl-3">
+                        <div class="footer-widget mb-100">
+                            <div class="widget-title">
+                                <h6>Why Choose us?</h6>
+                            </div>
+                            <p>Fusce nec ante vitae lacus aliquet vulputate. Donec scelerisque accumsan molestie. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras sed accumsan neque.</p>
+                            <a href="#" class="btn faith-btn mt-70">Sunday Workship: 10:30 AM</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- ##### Footer Area Start ##### -->
+
+    <!-- ##### All Javascript Script ##### -->
+    <!-- jQuery-2.2.4 js -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/bootstrap/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <!-- All Plugins js -->
+    <script src="js/plugins/plugins.js"></script>
+    <script src="js/plugins/audioplayer.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+</body>
+
+</html>
